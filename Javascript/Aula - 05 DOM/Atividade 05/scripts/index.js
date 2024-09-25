4a8b2b8bc8d573178d922121b8d6ab82172f2962
leoice01 <leonardoandrade893@gmail.com>
2024-09-25 12:01:14 -0300

    Repetição e Array

---
+++ Javascript/Aula - 05 DOM/Atividade 05/scripts/index.js
@@ -0,0 +1,13 @@
+function filmes18(){
+    var filmes = [`Deadpool & Wolverine`,`Super Onze`,`Batman`,`Meu Malvado Favorito 6`,`Não fale o mal`,`Silvio`]
+    console.log(filmes)
+
+    filmes.forEach(categoria => {
+        document.getElementById("teste1").innerHTML += `<div class="categoria1">${categoria}</div>`
+    });
+}
+
+function tabuada(){
+    var input_test
+    
+}
