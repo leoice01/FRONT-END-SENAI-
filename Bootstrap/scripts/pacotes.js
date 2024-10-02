39ab43ebfe096880c566f0e1f6e78d19e44f977f
leoice01 <leonardoandrade893@gmail.com>
2024-10-02 12:13:36 -0300

    Paw Shop

--- Bootstrap/scripts/pacotes.js
+++ Bootstrap/scripts/pacotes.js
@@ -1,28 +1,27 @@
-class Pacotes{
-
-        constructor(srcImg,local,descricao,preco,pacotes){
-            this.srcImg = srcImg;
-            this.local = local;
-            this.descricao = descricao;
-            this.preco = preco;
-            this.pacotes = pacotes;
-        }
+class Pacote{
+    constructor(srcImg,local,descricao,preco,pacotes){
+        this.srcImg = srcImg;
+        this.local = local;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.pacotes = pacotes;
+    }
 }
 
 var todosPacotes = []
-todosPacotes.push(new Pacotes("#","Nordeste","Melhor comida brasileira!",350,["Pacote 3 Dias","Pacote 5 dias"]))
+todosPacotes.push(new Pacote("https://www.passagenspromo.com.br/blog/wp-content/uploads/2021/05/estados-do-nordeste-740x415.jpg","Nordeste","Melhor comida brasileira!",350,["Pacote 3 dias","Pacote 5 dias"]))
 
 todosPacotes.forEach(cadaPacote => {
     document.getElementById("catalogo").innerHTML += `
-    <div class="card" style="width: 18rem;">
-        <img src="${cadaPacote.srcImg}" class="card-img-top" alt="...">
-        <div class="card-body">
-        <h5 class="card-title">${cadaPacote.local}</h5>
-        <p class="${cadaPacote.descricao}"></p>
-        <p class="${cadaPacote.preco}"></p>
-        <p class="${cadaPacote.pacotes}"></p>
-        <a href="#" class="btn btn-primary">COMPRAR PACOTE</a>
-    </div>
-</div>
-`
+        <div class="card" style="width: 18rem;">
+            <img src="${cadaPacote.srcImg}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${cadaPacote.local}</h5>
+                <p class="card-text">${cadaPacote.descricao}</p>
+                <p class="card-text">${cadaPacote.preco}</p>
+                <p class="card-text">${cadaPacote.pacotes}</p>
+                <a href="#" data-bs-toggle="modal" data-bs-target="#sobrepacote" class="btn btn-primary">COMPRAR PACOTE</a>
+            </div>
+        </div>
+    `
 });
